7f7a7bedb617344dabdc3546b4c874594555f729
dwhyq <dwhyq@outlook.com>
2022-11-04 14:58:52 +0100

    submition: added the submit button

--- src/component/contact-page/contact.jsx
+++ src/component/contact-page/contact.jsx
@@ -4,10 +4,10 @@ import i4gLogo from "../all-images/I4G.png";
 import "./contact.css";
 
 class contact extends Component {
-
   handleSubmit = (event) => {
-    event.preventDefault()
-  }
+    alert(`sent`);
+    event.preventDefault();
+  };
   render() {
     const { name } = this.props;
     return (
@@ -23,28 +23,43 @@ class contact extends Component {
             <div className="first-lastname">
               <div className="firstname">
                 <label for="name">First name</label>
-                <div id="first_name">
-                  <input type="text" placeholder="Enter your first name" />
+                <div id="first_name_">
+                  <input
+                    id="first_name"
+                    type="text"
+                    placeholder="Enter your first name"
+                  />
                 </div>
               </div>
               <div id="lastname">
                 <label for="name">Last name</label>
-                <div id="last_name">
-                  <input type="text" placeholder="Enter your last name" />
+                <div id="last_name_">
+                  <input
+                    id="last_name"
+                    type="text"
+                    placeholder="Enter your last name"
+                  />
                 </div>
               </div>
             </div>
 
             <div className="email">
               <label for="email">Email</label>
-              <div id="email">
-                <input type="email" placeholder="yourname@gmail.com" />
+              <div id="email_">
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="yourname@gmail.com"
+                />
               </div>
             </div>
             <div className="Message">
               <label for="text">Message</label>
-              <div id="message">
-                <textarea placeholder="Send me a message and i'll reply you as soon as possible"></textarea>
+              <div id="message_">
+                <textarea
+                  id="message"
+                  placeholder="Send me a message and i'll reply you as soon as possible"
+                ></textarea>
               </div>
             </div>
 
@@ -55,16 +70,16 @@ class contact extends Component {
               </label>
             </div>
 
-            <div className="submit_btn">
-              <p>Send message</p>
-            </div>
+            <button id="btn__submit">
+              <div className="submit_btn">Send message</div>
+            </button>
           </form>
         </div>
         <footer>
-            <img src={zuriLogo} className="zuriLogo" alt="zuri__logo" />
-            <h2>HNG Internship 9 Frontend Task</h2>
-            <img src={i4gLogo} alt="i4g__logo" />
-          </footer>
+          <img src={zuriLogo} className="zuriLogo" alt="zuri__logo" />
+          <h2>HNG Internship 9 Frontend Task</h2>
+          <img src={i4gLogo} alt="i4g__logo" />
+        </footer>
       </div>
     );
   }
